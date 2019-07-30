ca50b4aeb279794132cc9479aaef7044d467f891
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-30 16:53:06 +0100

    ISecurity implemented in Playground and tested.

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(14, 100, 50.00);
+        visitor2 = new Visitor(16, 150, 50.00);
     }
 
     @Test
@@ -26,5 +31,16 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void willAllowVisitorLessThan15 () {
+        assertEquals(true, playground.isAllowedTo(visitor1));
+
+    }
+
+    @Test
+    public void willNotAllowVisitorAbove15 () {
+        assertEquals(false, playground.isAllowedTo(visitor2));
     }
 }
