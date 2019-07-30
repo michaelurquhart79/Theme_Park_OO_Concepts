e6e2827282b26d228d12ca978eece4d954906a40
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-30 17:23:32 +0100

    ISecurity implemented in RollerCoaster and tested.

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,25 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor youngTall;
+    Visitor youngShort;
+    Visitor oldTall;
+    Visitor oldShort;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        youngTall = new Visitor(10, 150, 20.00);
+        youngShort = new Visitor(10, 130, 20.00);
+        oldTall = new Visitor( 15, 210, 20.00);
+        oldShort = new Visitor(20, 130, 20.00);
     }
 
     @Test
@@ -28,4 +37,26 @@ public class RollercoasterTest {
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
     }
+
+    @Test
+    public void notAllowedIfUnderAgeButTallEnough(){
+        assertEquals(false, rollerCoaster.isAllowedTo(youngTall));
+    }
+
+    @Test
+    public void notAllowedIfUnderAgeAndNotTallEnough(){
+        assertEquals(false, rollerCoaster.isAllowedTo(youngShort));
+    }
+
+    @Test
+    public void notAllowedIfOverAgeButNotTallEnough(){
+        assertEquals(false, rollerCoaster.isAllowedTo(oldShort));
+    }
+
+    @Test
+    public void AllowedIfOverAgeAndTallEnough(){
+        assertEquals(true, rollerCoaster.isAllowedTo(oldTall));
+    }
+
+
 }
