a17b832394d3157ced100a6bf8ebb97af1723a21
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-30 19:44:00 +0100

    ITicket interface implemented into Dodgems class and tested.

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,25 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class Dodgems extends Attraction implements ITicketed {
+
+    private double standardPrice;
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+        this.standardPrice = 4.50;
+    }
+
+    public double defaultPrice() {
+        return this.standardPrice;
+    }
+
+    public double priceFor(Visitor visitor) {
+        if (visitor.getAge() < 12){
+            return defaultPrice() / 2;
+        }
+        return defaultPrice();
     }
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor youngVisitor;
+    Visitor oldVisitor;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         dodgems = new Dodgems("Bumper Cars", 5);
+        youngVisitor = new Visitor(9, 130, 20.00);
+        oldVisitor = new Visitor(14, 150, 20.00);
     }
 
 
@@ -28,5 +33,20 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void canGetDefaultPrice() {
+        assertEquals(4.50, dodgems.defaultPrice(), 0.001);
+    }
+
+    @Test
+    public void canGetCheapEntryIfYoung() {
+        assertEquals(2.25, dodgems.priceFor(youngVisitor), 0.001);
+    }
+
+    @Test
+    public void hasToPayFullPriceIfOld() {
+        assertEquals(4.50, dodgems.priceFor(oldVisitor), 0.001);
     }
 }
